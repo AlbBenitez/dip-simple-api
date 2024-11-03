8d86256f35d1be2f488992ec92ffc26a5f7b8c50
Alberto <alb.benitez.ruiz@gmail.com>
2024-11-03 16:42:24 +0000

    added variables

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Attempt to deploy changing api ';
+    return 'Hello World! Attempt to deploy changing api with variables ';
   }
 }
